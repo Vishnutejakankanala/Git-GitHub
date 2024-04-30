21b53c17ec311af29c113400260caf9e9c252cf1
EC2 Default User <ec2-user@ip-10-0-10-71.ap-south-1.compute.internal>
2024-04-30 06:40:51 +0000

    this is my first commit

--- var.sh
+++ var.sh
@@ -45,18 +45,3 @@ echo ${just[2]}
 #in array if we give input 2 then it will print 3rd statement if we give the numers starts from zero string[] cars = {"volvo","MW","Ford"} Int[] mynum = {0,1,2,3}
 
 
-read -p "what you want to print : " vishnu
-echo $vishnu
-echo "what you want to print" hi
-read $hi
-x=10
-y=$((12+3))
-echo "$x"
-echo "$y"
-echo "$((x+y))"
-read -a just
-echo ${just[0,2,5]}
-echo ${just}
-echo ${just[0]}
-echo ${just[@]}
-echo ${just[@]}
